8a9784ad7bf48bc5fd5501b3924a69c8f700f77b
Ahmed Hajiyev <hajiyev.ahmed01@gmail.com>
2023-03-24 23:00:09 +0400

    added model

--- src/main/java/kodlama/io/rentACar/entities/concretes/Model.java
+++ src/main/java/kodlama/io/rentACar/entities/concretes/Model.java
@@ -6,30 +6,36 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-
-@Table(name="brands")
-@Data
+@Table(name="models")
+@Getter
+@Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
-public class Brand {
-	
+public class Model {
 	@Id
-	@GeneratedValue(strategy= GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name="id")
-	private int id;
+	private int Id;
 	
 	@Column(name="name")
 	private String name;
 	
-	@OneToMany(mappedBy = "brand")
-	private List<Model> models;
+	@ManyToOne
+	@JoinColumn(name="brand_id")
+	private Brand brand;
 	
+	@OneToMany(mappedBy = "model")
+	private List<Car> cars;
+
 }
--- src/main/java/kodlama/io/rentACar/webApi/controllers/BrandsController.java
+++ src/main/java/kodlama/io/rentACar/webApi/controllers/BrandsController.java
@@ -8,10 +8,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import jakarta.validation.Valid;
 import kodlama.io.rentACar.business.abstracts.BrandService;
 import kodlama.io.rentACar.business.requests.CreateBrandRequest;
 import kodlama.io.rentACar.business.requests.UpdateBrandRequest;
@@ -38,14 +40,12 @@ public class BrandsController {
 	
 	@PostMapping()
 	@ResponseStatus(code=HttpStatus.CREATED)
-	public void add(CreateBrandRequest createBrandRequest) {
-		System.out.println("**");
-		System.out.println(createBrandRequest);
+	public void add(@RequestBody @Valid CreateBrandRequest createBrandRequest) {
 		this.brandService.add(createBrandRequest);
 	}
 	
 	@PutMapping()
-	public void update(UpdateBrandRequest updateBrandRequest) {
+	public void update(@RequestBody UpdateBrandRequest updateBrandRequest) {
 		this.brandService.update(updateBrandRequest);
 	}
 	
